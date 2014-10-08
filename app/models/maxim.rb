223acdb023b15dbf09c15cf6a9903a94985395d0
Gk <gk@gkparishphilp.com>
2014-10-08 09:29:47 -0700

    - layout + maxim

---
+++ app/models/maxim.rb
@@ -0,0 +1,13 @@
+class Maxim < SwellMedia::Media
+
+	# a short quote, saying, or aphorism... 
+	# sometimes mine, sometimes attributed
+	# wiht own template/styling....
+	# thinking large font + cover bg image
+	# Poetry + Philosophy: I just can't resist these...
+
+	private
+		def allow_blank_title
+			true
+		end
+end
